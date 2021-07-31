df1eb30f8e26af2f02a79fa678494f7f7dea9f99
Benjamin De Mot <benjamindm@hotmail.fr>
2021-07-31 14:07:50 +0200

    style; still implementing basic design

--- src/App.vue
+++ src/App.vue
@@ -1,88 +1,31 @@
 <template>
-  <div class="nav">
-    <div class="nav__burger">
-      <div class="nav__burger--bar">
-      </div>
-      <div class="nav__burger--bar">
-      </div>
-      <div class="nav__burger--bar">
-      </div>
-    </div>
-    <div class="nav__drawer">
-      <router-link to="/">Accueil</router-link>
-      <router-link to="/about">Textes</router-link>
-      <router-link to="/about">Leçons</router-link>
-      <router-link to="/about">Audio</router-link>
-    </div>
-  </div>
+  <drawer-menu/>
   <router-view/>
 </template>
 
+<script>
+// @ is an alias to /src
+import DrawerMenu from '@/components/DrawerMenu.vue'
+
+export default {
+  components: {
+    DrawerMenu
+  }
+}
+</script>
+
 <style lang="scss">
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-.nav {
-  padding: 30px;
-
-  &__drawer{
-    background-color: rgb(255, 253, 253);
-    display: flex;
-    flex-direction: column;
-    justify-content: space-evenly;
-    width: 70%;
-    height: 70%;
-    position: absolute;
-    right: -100%;
-    top: 60px;
-    box-shadow: -2px 0px 6px 1px rgba(0, 0, 0, 0.2);
-    transition: all .5s;
-    & a{
-      color: #014900;
-      font-weight: bold;
-
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      height: 50px;
-
-      text-decoration: none;
-      font-size: 2em;
-        &.router-link-exact-active {
-          color: #42b983;
-    }
-    }
-  }
-
-  &__burger{
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-direction: column;
-    border-radius: 50%;
-    width: 40px;
-    height: 40px;
-    background-color: #AFF0B8;
-    position: absolute;
-    right: 10px;
-    top: 10px;
-    &:hover + .nav__drawer{
-      position: absolute;
-      right: 0;
-    }
-
-    &--bar{
-      display: block;
-      height: 2px;
-      width: 25px;
-      background-color: #29BB3E;
-      margin: 2px;
-    }
-  }
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,43 @@
 <template>
   <div class="home">
-    <img alt="burhaniya logo" width="150" src="../assets/logo.jpg">
+    <img alt="burhaniya logo" width="130" src="../assets/logo.jpg">
     <h1>Hadrapp</h1>
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
+  <main class="menus">
+    <button class="menus__menu">
+      Textes
+    </button>
+    <button class="menus__menu">
+      Leçons
+    </button>
+    <button class="menus__menu">
+      Audio
+    </button>
+  </main>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+  name: 'Home'
 }
 </script>
+
+<style lang="scss" scoped>
+  .menus{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-top: 30px;
+    &__menu {
+      background-color: #AFF0B8;
+      height: 60px;
+      width: 70%;
+      margin: 10px;
+      font-size: 2em;
+      border: 0;
+      border-radius: 100px;
+    }
+  }
+</style>
